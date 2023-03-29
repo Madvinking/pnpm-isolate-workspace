82c85dcebe6403a5a709a3e3e8593150cc4857d2
nir winkler <madvinking@gmail.com>
2023-03-29 09:27:55 +0300

    update to pnpm 8

--- src/params.js
+++ src/params.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const fs = require('fs');
 
-const { default: getAllWorkspaces } = require('@pnpm/find-workspace-packages');
+const { findWorkspacePackages } = require('@pnpm/find-workspace-packages');
 
 async function getWorkspaces(workspaceRoot) {
-  return (await getAllWorkspaces(workspaceRoot)).reduce((acc, { dir, manifest: { name } }) => {
+  return (await findWorkspacePackages(workspaceRoot)).reduce((acc, { dir, manifest: { name } }) => {
     acc[name] = {
       location: dir,
     };
